61a3549d5c7cf50adf567df2f1c6bf50591a288e
LittleQ <littleq0903@gmail.com>
2013-03-19 16:42:00 -0700

    exit when didn't get the filename as a argument

--- docx2html/tool.py
+++ docx2html/tool.py
@@ -2,8 +2,11 @@ from docx2html.core import convert
 
 if __name__ == "__main__":
     import sys
-    import_filename = sys.argv[1]
-    print sys.argv[1]
+    try:
+        import_filename = sys.argv[1]
+    except:
+        print 'Please provide the filename, like `python -m docx2html.tool foo.docx`'
+        sys.exit(1)
     html = convert(import_filename)
     new_html = open(import_filename.replace('.docx','.html'), 'w')
     new_html.write(html)
